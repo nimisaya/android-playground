a281e331dfc30da5e09bea9187baa6f8f01e8ef9
nimisaya <nimisaya@outlook.com>
2021-11-30 22:44:34 +1100

    Add examples of ImageView TintMode

---
+++ ImagePlayground/app/src/main/java/com/nimisaya/imageplayground/LearnTintMode.kt
@@ -0,0 +1,16 @@
+package com.nimisaya.imageplayground
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LearnTintMode : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_learn_tint_mode, container, false)
+    }
+}
