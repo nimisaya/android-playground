1defa9c3bf5b020ac510d0f9dbb1d4bc97889ef3
nimisaya <nimisaya@outlook.com>
2021-12-13 12:19:55 +1100

    Display profile picture

--- MessageUI/app/src/main/java/com/nimisaya/messageui/MainActivity.kt
+++ MessageUI/app/src/main/java/com/nimisaya/messageui/MainActivity.kt
@@ -3,29 +3,57 @@ package com.nimisaya.messageui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.nimisaya.messageui.ui.theme.MessageUITheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Nimi")
+            MessageCard(Message("Nimi", "G\'day people"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row {
+        ProfilePicture()
+
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(Modifier.padding(16.dp)) {
+            Text(text = msg.author, fontWeight = FontWeight.Bold)
+            Text(text = msg.body)
         }
     }
 }
 
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "G\'day $name!")
+fun ProfilePicture(){
+    Box() {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture background colour",
+        )
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "Contact profile picture",
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MessageCard(name = "Nimi")
+    MessageCard(Message(author = "PersonName", body = "I\'m a Message"))
 }
