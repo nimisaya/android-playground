5f174a0aafa90a1bd67a504e2d25fc0d599e5189
nimisaya <nimisaya@outlook.com>
2021-12-15 11:19:23 +1100

    Add shape and elevation to message body

--- MessageUI/app/src/main/java/com/nimisaya/messageui/MainActivity.kt
+++ MessageUI/app/src/main/java/com/nimisaya/messageui/MainActivity.kt
@@ -8,12 +8,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nimisaya.messageui.ui.theme.MessageUITheme
@@ -45,10 +45,14 @@ fun MessageCard(msg: Message) {
                 style = MaterialTheme.typography.subtitle2,
                 color = MaterialTheme.colors.secondaryVariant
                 )
-            Text(
-                text = msg.body,
-                style = MaterialTheme.typography.subtitle2,
-            )
+            Spacer(modifier= Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 8.dp),
+                    style = MaterialTheme.typography.subtitle2,
+                )
+            }
         }
     }
 }
