18872dfe0baee8bcfebfa208edb53b5c7386c535
nimisaya <nimisaya@outlook.com>
2021-12-20 23:06:09 +1100

    Add spring animation and update colours

--- AnimatedList/app/src/main/java/com/nimisaya/animatedlist/MainActivity.kt
+++ AnimatedList/app/src/main/java/com/nimisaya/animatedlist/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nimisaya.animatedlist.ui.theme.AnimatedListTheme
@@ -68,18 +72,36 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }){
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false)}
+    var expanded by remember { mutableStateOf(false)}
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+
+    )
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)){
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
                 Text(text = "Hello, ")
-                Text(text = "$name!")
+                Text(
+                    text = "$name!",
+                    style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
-            OutlinedButton(onClick = {expanded.value = !expanded.value}) {
-                Text(if (expanded.value) "Show less" else "Show more")
+            OutlinedButton(onClick = {expanded = !expanded}) {
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
