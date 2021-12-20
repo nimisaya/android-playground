6fb3834a65dce3e064a3065ecd089731af1fc1a3
nimisaya <nimisaya@outlook.com>
2021-12-20 22:20:30 +1100

    reate onboarding screen

--- AnimatedList/app/src/main/java/com/nimisaya/animatedlist/MainActivity.kt
+++ AnimatedList/app/src/main/java/com/nimisaya/animatedlist/MainActivity.kt
@@ -3,11 +3,10 @@ package com.nimisaya.animatedlist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +24,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "People")){
+fun MyApp(){
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit = {}){
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Yo! Check out an animated list")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "People")){
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
@@ -35,6 +64,7 @@ fun MyApp(names: List<String> = listOf("World", "People")){
 
 @Composable
 private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false)}
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -44,15 +74,24 @@ private fun Greeting(name: String) {
                 Text(text = "Hello, ")
                 Text(text = "$name!")
             }
-            OutlinedButton(onClick = {}) {
-                Text("Show more")
+            OutlinedButton(onClick = {expanded.value = !expanded.value}) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
 }
 
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320, name = "Onboarding")
+@Composable
+fun OnboardingPreview() {
+    AnimatedListTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, name = "Animated List")
 @Composable
 fun DefaultPreview() {
     AnimatedListTheme {
